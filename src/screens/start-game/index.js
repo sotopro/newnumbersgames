5139c92d64ec2fbc072cbe6bd7adc6fa647ac421
sotopro <dasg.1993@gmail.com>
2022-11-14 19:24:12 -0500

    [feature] - create start game screen layout and style

--- src/screens/start-game/index.js
+++ src/screens/start-game/index.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 import { styles } from './styles';
 
 const StartGame = () => {
     return (
         <View style={styles.container}>
-            <Text>Start Game</Text>
+            <Text style={styles.title}>Let's start!</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>Select a number</Text>
+                <TextInput style={styles.input} placeholder="0" />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    title="Reset"
+                    onPress={() => null}
+                    color='#A7BED3'
+                />
+                <Button 
+                    title="Confirm"
+                    onPress={() => null}
+                    color='#A7BED3'
+                />
+            </View>
         </View>
     )
 }
--- src/screens/start-game/styles.js
+++ src/screens/start-game/styles.js
@@ -3,7 +3,39 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
-    }
+        marginVertical: 15,
+    },
+    title: {
+        fontSize: 18,
+        color: '#212121',
+        textAlign: 'center',
+        paddingVertical: 20,
+    },
+    label: {
+        fontSize: 15,
+        color: '#212121',
+        paddingVertical: 5,
+        textAlign: 'center',
+    },
+    inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: 20,
+    },
+    input: {
+        width: '100%',
+        borderBottomColor: '#A7BED3',
+        borderBottomWidth: 1,
+        minWidth: 70,
+        fontSize: 22,
+        paddingVertical: 10,
+        textAlign: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '70%',
+        justifyContent: 'space-around',
+        marginTop: 20,
+    },
 });
